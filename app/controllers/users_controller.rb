7aae693c0d5512300eb1d3b562df5bb3cec6513e
keisuke.okamura <cointoss69@icloud.com>
2023-10-15 11:06:34 +0000

    kadai-microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
     @pagy, @followers = pagy(@user.followers)
     counts(@user)
   end
+  
+  def likes
+    @user = User.find(params[:id])
+    @pagy, @likes = pagy(@user.likes)
+    counts(@user)
+  end
 
   private
 
